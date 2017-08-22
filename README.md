ddb6233897f67aaba291e1779aeeb4fb888260a4
saintjames <jamess1@umbc.edu>
2017-08-22 16:51:29 -0400

    Initial

--- README.md
+++
@@ -1,3 +0,0 @@
-# healthcare
-
-This is modified / extended code, originating from offermend/medicare-demo and kiwenlau/hadoop-cluster-docker here on github. 
--- hadoop/scripts/to-master.sh
+++
@@ -1,8 +0,0 @@
-if [ -z "$1" ]
-  then
-    echo "No Cluster Number Supplied"
-    exit 1
-fi
-
-# get into hadoop master container                                                                                                                                                                                
-sudo docker exec -it hadoop${1}-master bash
