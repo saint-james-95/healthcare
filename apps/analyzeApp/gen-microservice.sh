a8382916bf7731d365e9d6b7cd7e76c4a7c6ba7e
team-lotta-cores <jmsheehan2197@gmail.com>
2017-09-01 12:32:31 -0400

    Update gen-microservice.sh

--- apps/analyzeApp/gen-microservice.sh
+++ apps/analyzeApp/gen-microservice.sh
@@ -1,6 +1,1 @@
-THIS_DIR=$PWD
-cd ~/Use-Case-Healthcare/environment/hadoop/scripts
-./configure.sh 2 10
-./build-configured-images.sh 2
-cd $THIS_DIR
-sudo docker build -t app:analyze .
+sudo docker build -t nbdif/healthcare:app_analyze .
