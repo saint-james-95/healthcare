c35b9486d42437fa0c57631c303b1f35e2cd2bb5
team-lotta-cores <jmsheehan2197@gmail.com>
2017-09-13 16:38:37 -0400

    Update build-base-images.sh

--- environment/hadoop/scripts/build-base-images.sh
+++ environment/hadoop/scripts/build-base-images.sh
@@ -6,11 +6,11 @@ cd $HOME/Use-Case-Healthcare/environment/hadoop/dockerfiles
 
 #Base image, shared by slave and master
 cd base-slave
-docker build -t environment:base-slave .
+docker build -t nbdif/healthcare:hadoop_slave_base .
 
 #Extra programs for the master
 cd ..
 cd base-master
-docker build -t environment:base-master .
+docker build -t nbdif/healthcare:hadoop_master_base .
 
 cd $THIS_DIR
